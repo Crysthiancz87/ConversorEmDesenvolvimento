08e885a4d04d7e71f5d49f61b66a3b5a2b6bc467
Crysthiancz87 <crysthiancz87@gmail.com>
2024-05-01 13:54:51 +0200

    bugCorrigido

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const convertButton = document.querySelector(".convert-button")
 function convertValues(){
     const inputCurrencyValue = document.querySelector(".input-currency").value
 
-    const currencyValueToConvert = document.querySelector("currency-value-to-convert")
-    const currencyValueConverted = document.querySelector("currency-value")
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
 
     const dolarToday = 5.2
     
